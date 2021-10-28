b1fefd26a7195e1b9b9a33211a734bf0501f936a
cmhyles <36858511+cmhyles@users.noreply.github.com>
2021-10-28 03:10:45 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require('plugins')
+require('keymappings')
+require('nv-cmp')
+require('lsp.lua-lsp')
+require('lsp.clangd-lsp')
+
+vim.o.wrap = false
+vim.o.completeopt = "menuone,noselect,menu"
+
---
+++ lua/plugins.lua
@@ -0,0 +1,25 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function() require'nvim-tree'.setup {} end
+}
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+end)
+
